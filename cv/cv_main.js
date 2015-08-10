1b00ffe3c6421ff623549f0663db812fdabf4e6f
Alexander Vinogradov <artfabrique@gmail.com>
2015-08-10 04:11:46 +0300

    Google Analytics Yandex Metrics

--- cv/cv_main.js
+++ cv/cv_main.js
@@ -12,6 +12,7 @@ function CV_checkPreload() {
 function CV_OnAxure_Loaded() {
 	console.log("CV_OnAxure_Loaded");
 	$axure.setGlobalVariable("ready",(CV_ready?"1":"0"));
+
 }
 
 function CV_Init() {
@@ -19,6 +20,44 @@ function CV_Init() {
 	$("head").append('<link type="text/css" rel="stylesheet" href="cv/cv_style.css">');
 	$.getScript("cv/soundmanager/soundmanager2.js").done(CV_InitSoundManager);
 	$('[data-label*="^"]').css('pointer-events', 'none');
+
+
+// Google analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-66126184-1', 'auto');
+	ga('send', 'pageview');
+
+	
+// Yandex metrics
+	(function (d, w, c) {
+	    (w[c] = w[c] || []).push(function() {
+	        try {
+	            w.yaCounter31868886 = new Ya.Metrika({
+	                id:31868886,
+	                clickmap:true,
+	                trackLinks:true,
+	                accurateTrackBounce:true,
+	                webvisor:true,
+	                trackHash:true
+	            });
+	        } catch(e) { }
+	    });
+
+	    var n = d.getElementsByTagName("script")[0],
+	        s = d.createElement("script"),
+	        f = function () { n.parentNode.insertBefore(s, n); };
+	    s.type = "text/javascript";
+	    s.async = true;
+	    s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+	    if (w.opera == "[object Opera]") {
+	        d.addEventListener("DOMContentLoaded", f, false);
+	    } else { f(); }
+	})(document, window, "yandex_metrika_callbacks");
 	//$("head").append('<script type="text/javascript" src="http://artfabrique.github.io/_cv/soundmanager/soundmanager2.js"></script>');
 }
 
